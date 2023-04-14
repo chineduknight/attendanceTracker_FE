4f718701364598db312e0fb4dd90140928b924f9
Sochima <ezeobisochima@gmail.com>
2023-04-14 11:34:23 +0100

    added some features on the add organisation list and orglist

--- src/pages/Orglist.tsx
+++ src/pages/Orglist.tsx
@@ -1,9 +1,15 @@
-import { Box, Flex, useColorModeValue, Button, Text } from "@chakra-ui/react";
+import {
+  Box, Flex, useColorModeValue, Button, Text, Stack
+} from "@chakra-ui/react";
+import db from './../../db.json';
+// import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { PROTECTED_PATHS } from "routes/pagePath";
 
 const OrgList = () => {
   const navigate = useNavigate();
+  // const [orglist, setOrglist] = useState(true)
+
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.50", "gray.800")}>
       <Flex
@@ -15,6 +21,24 @@ const OrgList = () => {
         <Text color="#fff">Attendance Tracker</Text>
         <Button onClick={() => navigate(PROTECTED_PATHS.ADD_ORG)}>+</Button>
       </Flex>
+      <Stack
+        spacing={4}
+        w={"full"}
+        maxW={"md"}
+        bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        boxShadow={"lg"}
+        p={6}
+        my={12}
+        mx='auto'
+      >
+        <Text fontWeight='bold'>No organisation yet</Text>
+        {/* {orglist
+          ? <Text fontWeight='bold'>organisation yet</Text>
+          : (<Text fontWeight='bold'>No organisation yet</Text>)
+        } */}
+      </Stack>
+
     </Box>
   );
 };
