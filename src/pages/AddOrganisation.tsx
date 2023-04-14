8d7ddc1d898cd0e93dedee2555c0ebba891e40cc
Sochima <ezeobisochima@gmail.com>
2023-04-14 16:33:25 +0100

    input to the console

--- src/pages/AddOrganisation.tsx
+++ src/pages/AddOrganisation.tsx
@@ -10,6 +10,7 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import { PROTECTED_PATHS } from "routes/pagePath";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQueryWrapper } from 'services/api/apiHelper';
 import { orgRequest } from 'services/api/request';
@@ -17,7 +18,20 @@ import { FaArrowCircleLeft } from 'react-icons/fa'
 const AddOrganisation = () => {
   const navigate = useNavigate();
   const { data } = useQueryWrapper(["my-key"], orgRequest.ORG);
-  console.log("data:", data)
+  // console.log("data:", data)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function newOrganisation(e) {
+    let input = e.target
+    if (input.name === 'email') {
+      setEmail(input.value)
+    } else if (input.name === 'password') {
+      setPassword(input.value)
+    }
+  }
+
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.50", "gray.800")}>
 
@@ -33,11 +47,11 @@ const AddOrganisation = () => {
       </Flex>
       <Button
         onClick={() => navigate(PROTECTED_PATHS.ALL_ORG)}
-        // bg={"blue.400"}
+
         mt='10px'
         ml='10px'
         color={"white"}
-        // w="200px"
+
         _hover={{
           bg: "blue.500",
         }}
@@ -60,21 +74,36 @@ const AddOrganisation = () => {
           p={6}
           my={12}
         >
-          <FormControl id="email" isRequired>
+          <FormControl
+            id="email"
+            isRequired>
             <FormLabel>Organisation Name</FormLabel>
             <Input
               placeholder="Seat of wisdom presidium"
               _placeholder={{ color: "gray.500" }}
               type="text"
+              name="email"
+              value={email}
+              onChange={newOrganisation}
             />
           </FormControl>
           <FormControl id="password">
             <FormLabel>Image Url</FormLabel>
-            <Input type="text" />
+            <Input
+              type="text"
+              name="password"
+              value={password}
+              onChange={newOrganisation}
+            />
           </FormControl>
           <Stack spacing={6}>
             <Button
-              onClick={() => navigate(PROTECTED_PATHS.ALL_ORG)}
+              onClick={() => {
+                console.log(`organisation registered with ${email} and ${password}`)
+
+                navigate(PROTECTED_PATHS.ALL_ORG)
+              }
+              }
               bg={"blue.400"}
               color={"white"}
               _hover={{
@@ -86,7 +115,7 @@ const AddOrganisation = () => {
           </Stack>
         </Stack>
       </Flex>
-    </Box>
+    </Box >
   );
 };
 
